e4ae4c448b4c68e3e5d63e4c3b479ac5850256c6
Vlad <gresko.97@mail.ru>
2024-01-20 17:30:49 +0300

    Add project files.

---
+++ Task1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    internal class Program
+    {
+        
+        static void Factorial(int n)
+        {
+            long fact = 1;
+            for (int i = 1; i <= n; i++)
+            {
+                fact *= i;
+                Thread.Sleep(100);
+            }
+            Console.WriteLine(fact);
+        }
+
+      
+        static async void FactorialAsync(int n)
+        {
+             await Task.Run(() => Factorial(n));
+        }
+
+      
+
+
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+           FactorialAsync(n);
+            Console.ReadKey();
+        }
+
+
+    }
+}
